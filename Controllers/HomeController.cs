9980487f3cfee74981e85ac9baec2823350b1dcb
Jonatan-ESG <jona.sandovalguerra@gmail.com>
2024-11-09 11:32:15 -0600

    :construction_worker: Se agregó implementación de gráficas base

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,12 +18,20 @@ namespace UspgPOS.Controllers
             if (User.Identity.IsAuthenticated)
             {
                 ViewData["Layout"] = "_LayoutLogged"; // Layout para usuarios logueados
+                var detallesEmpleados = new List<DetallesEmpleado>
+                {
+                    new DetallesEmpleado { Departamento = "IT", NivelExperiencia = 5, CalificacionRendimiento = 8.5, SalarioPromedio = 80000, TasaRotacion = 12.5 },
+                    new DetallesEmpleado { Departamento = "RRHH", NivelExperiencia = 3, CalificacionRendimiento = 7.2, SalarioPromedio = 60000, TasaRotacion = 8.3 },
+                    new DetallesEmpleado { Departamento = "Marketing", NivelExperiencia = 4, CalificacionRendimiento = 7.8, SalarioPromedio = 70000, TasaRotacion = 10.1 },
+                    new DetallesEmpleado { Departamento = "Ventas", NivelExperiencia = 6, CalificacionRendimiento = 8.0, SalarioPromedio = 75000, TasaRotacion = 15.0 }
+                };
+                return View(detallesEmpleados);
             }
             else
             {
                 ViewData["Layout"] = "_Layout"; // Layout para usuarios no logueados
+                return View();
             }
-            return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
